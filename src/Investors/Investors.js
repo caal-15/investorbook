0e5b86128bb9e272c6dd533da3221bc1b40aa414
Carlos González <caal.0522@gmail.com>
2020-12-14 17:03:49 -0500

    Tweaks Investor List a little.

--- src/Investors/Investors.js
+++ src/Investors/Investors.js
@@ -7,6 +7,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { useQuery } from "@apollo/client";
 
 import { GET_INVESTORS } from "./queries";
@@ -21,39 +22,45 @@ const Investors = () => {
     variables: { limit: pageSize, offset: page * pageSize },
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  if (data.investor.length === 0) return <p>The database is empty!</p>;
-
   return (
     <>
       <Typography align="left" variant="h3" component="h2" gutterBottom>
         Investors
       </Typography>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>NAME</TableCell>
-            <TableCell>INVESTMENTS</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.investor.map((investor) => (
-            <InvestorRow key={investor.id} investor={investor} />
-          ))}
-        </TableBody>
-      </Table>
-      <Box m={1} />
-      <Grid container justify="flex-end">
-        <Grid item>
-          <Pagination
-            pageSize={pageSize}
-            setPageSize={setPageSize}
-            page={page}
-            setPage={setPage}
-          />
-        </Grid>
-      </Grid>
+      {loading ? <CircularProgress /> : null}
+      {!loading && !error && data?.investor?.length ? (
+        <>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>NAME</TableCell>
+                <TableCell>INVESTMENTS</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.investor.map((investor) => (
+                <InvestorRow key={investor.id} investor={investor} />
+              ))}
+            </TableBody>
+          </Table>
+          <Box m={1} />
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Pagination
+                pageSize={pageSize}
+                setPageSize={setPageSize}
+                page={page}
+                setPage={setPage}
+              />
+            </Grid>
+          </Grid>
+        </>
+      ) : null}
+      {error ? (
+        <Typography color="error" variant="h5">
+          Ooops, Something went wrong, try reloading.
+        </Typography>
+      ) : null}
     </>
   );
 };
