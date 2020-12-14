ca56dc294449885b35215bb33108a6ab90af7d9a
Carlos González <caal.0522@gmail.com>
2020-12-14 11:45:27 -0500

    Adds initial app setup.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
-import Investors from './Investors'
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import "./App.css";
+
+import Investors from "./Investors";
 
 function App() {
   return (
     <div className="App">
-      <Investors />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/investors" />
+          </Route>
+          <Route exact path="/investors">
+            <Investors />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Investors/Investors.js
+++ src/Investors/Investors.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import { useQuery, gql } from '@apollo/client';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
 // Example of a component that uses apollo-client to fetch data.
 
 const GET_INVESTORS = gql`
   query GetInvestors {
-      investor(limit: 100) {
-          id
-          name
-      }
+    investor(limit: 100) {
+      id
+      name
+    }
   }
 `;
 
 export default () => {
-
   const { loading, error, data } = useQuery(GET_INVESTORS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  if (data.investor.length === 0) return <p>The database is empty!</p>
-  console.log(data)
+  if (data.investor.length === 0) return <p>The database is empty!</p>;
+  console.log(data);
 
   return data.investor.map(({ id, name }) => (
     <div key={id}>
@@ -28,4 +27,4 @@ export default () => {
       </p>
     </div>
   ));
-}
+};
